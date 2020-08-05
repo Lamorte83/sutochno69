42187bb79a2bf0ea8fccd0de260db105613a8887
Lamorte83 <69233184+Lamorte83@users.noreply.github.com>
2020-08-05 09:07:58 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,210 @@
+
+<?php require "header.php"; ?>
+
+
+
+
+<br>
+<div class="baner container-fluid">
+<br>
+<div class="d-flex flex-column">
+  <div class="p-2"></div>
+  
+  <div class="p-2 text-center">
+    
+  <H1 class="name animated bounceInDown">У нас всегда как дома...<br>
+    <!--<p>Почувствуй наслаждение от наших прикосновений..</p>-->
+  </H1>
+
+
+
+  </div>
+  <!--<div class="p-2 text-center">
+<h1 class="name animated bounceInLeft">+79157442257
+</h1>
+  </div>-->
+</div> 
+</div>
+<br>
+<!--   О нас     -->
+<div class="container-fluid animated bounceInUp">
+    <H1 class="title-first text-center " >Сервис посуточной аренды квартир в Твери</H1>
+</div>
+<br>
+
+<div class="container">
+<div class="row">
+<!--<div class="block-news col-sm-12 col-md-4 col-lg-4 text-center first animated bounceInLeft rounded">
+<h3 class="page-title">Текст</h3>
+<p class="text-page">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque consequuntur cum dolor vel in, debitis repellendus, numquam soluta sit, labore placeat corrupti? Harum earum, minus voluptatem id mollitia reprehenderit nulla!</p>
+</div>-->
+
+<div class="block-news  text-center first animated bounceInLeft rounded">
+<h3 class="page-title text-center">Квартира 1
+</h3>
+
+<p class="text-page">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+</div>
+
+
+<!--<div class="block-news col-sm-12 col-md-4 col-lg-4 text-center first animated bounceInLeft rounded">
+<h3 class="page-title">Loreipsum</h3>
+
+<p class="text-page">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quis ipsam iste architecto adipisci velit, aperiam eligendi optio. Vero voluptatibus blanditiis eligendi nobis nulla nesciunt quasi magni repudiandae est nemo!.</p>
+</div>-->
+
+
+</div>
+</div>
+
+<br>
+
+
+<div class="container-fluid">
+<div class="row">
+<div class="block-news col-sm-12 col-md-3 col-lg-3 text-center first animated bounceInLeft rounded">  
+<h3 class="page-title">Loreipsum</h3>
+<a href="">
+ <img src="img/skl70.jpg" class="img-fluid" alt="Responsive image"></a>
+
+<p class="text-page">
+Подарочные сертификаты на услуги нашего салона
+на СУММУ 2000 рублей ВСЕГО </p><br>
+
+</div>
+
+
+
+
+<div class="block-news col-sm-12 col-md-3 col-lg-3 text-center first animated bounceInLeft rounded">  
+<h3 class="page-title">Loreipsum</h3>
+<a href="">
+ <img src="img/skl70.jpg" class="img-fluid" alt="Responsive image"></a>
+
+<p class="text-page">
+Подарочные сертификаты на услуги нашего салона
+на СУММУ 2000 рублей ВСЕГО </p><br>
+
+
+
+</div>
+
+
+<div class="block-news col-sm-12 col-md-3 col-lg-3 text-center rounded">  
+<H3 class="page-title">1 комнатная квартира</h3>
+<a href="">
+ <img src="img/skl70.jpg" class="img-fluid" alt="Responsive image"></a>
+<p class="text-page">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+</div>
+
+
+
+<div class="block-news col-sm-12 col-md-3 col-lg-3 first animated bounceInRight rounded">  
+<h3 class="page-title text-center">Lore</h3>
+<a href="">
+ <img src="img/cent.jpg" class="img-fluid" alt="Responsive image"></a>
+<p class="text-page" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, 
+</p>
+</div>
+</div>
+</div>
+<br>
+<div class="container-fluid">
+<div class="row">
+<div class="block-news col-sm-12 col-md-3 col-lg-3 text-center first animated bounceInLeft rounded">  
+<h3 class="page-title">Loreipsum</h3>
+<a href="">
+ <img src="img/skl70.jpg" class="img-fluid" alt="Responsive image"></a>
+
+<p class="text-page">
+Подарочные сертификаты на услуги нашего салона
+на СУММУ 2000 рублей ВСЕГО </p><br>
+
+</div>
+
+
+
+
+<div class="block-news col-sm-12 col-md-3 col-lg-3 text-center first animated bounceInLeft rounded">  
+<h3 class="page-title">Loreipsum</h3>
+<a href="">
+ <img src="img/skl70.jpg" class="img-fluid" alt="Responsive image"></a>
+
+<p class="text-page">
+Подарочные сертификаты на услуги нашего салона
+на СУММУ 2000 рублей ВСЕГО </p><br>
+
+
+
+</div>
+
+
+<div class="block-news col-sm-12 col-md-3 col-lg-3 text-center rounded">  
+<H3 class="page-title">1 комнатная квартира</h3>
+<a href="">
+ <img src="img/skl70.jpg" class="img-fluid" alt="Responsive image"></a>
+<p class="text-page">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+</div>
+
+
+
+<div class="block-news col-sm-12 col-md-3 col-lg-3 first animated bounceInRight rounded">  
+<h3 class="page-title text-center">Lore</h3>
+<a href="">
+ <img src="img/cent.jpg" class="img-fluid" alt="Responsive image"></a>
+<p class="text-page" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, 
+</p>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--Контакты-->
+<?php require "footer.php";?>
+
+<script  src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"> </script>
+  <script  src="jq/jquery.fittext.js"> </script>
+  <script>
+    jQuery ( " #responsive_headline " ). fitText ();
+  </script>
+
+
+    
+
+
+  
+ 
